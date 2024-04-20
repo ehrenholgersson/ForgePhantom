c5877eb10f6878d941e5256f9831c3f7823157e7
ehrenholgersson <112737007+ehrenholgersson@users.noreply.github.com>
2024-04-20 11:08:06 +1000

    particel emitter on transport nodes

--- Scripts/TransportNode.cs
+++ Scripts/TransportNode.cs
@@ -13,22 +13,45 @@ public partial class TransportNode : Node3D
 	public float PathRadius { get => _pathradius; }
 	public float Speed { get => _speed; }
 	Area3D _entry;
+	GpuParticles3D _particles;
 
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
         _entry = GetNode<Area3D>("Entry");
+		_particles = GetNode<GpuParticles3D>("Particles");
 		_entry.BodyEntered += (target) => PickupObject(target);
 		if (DestinationNode != null)
 		{
 			_path = _destinationNode.GlobalPosition - GlobalPosition;
+			SetupParticles();
 		}
     }
 
+	void SetupParticles()
+	{
+		if (_particles != null)
+		{
+			ParticleProcessMaterial process = _particles.ProcessMaterial.Duplicate() as ParticleProcessMaterial;
+			if (process != null)
+			{
+				process.EmissionShapeScale = Vector3.One * _pathradius;
+				process.Direction = _path.Normalized();
+				process.Spread = 0;
+				process.InitialVelocityMax = _speed / process.ScaleMax;
+                process.InitialVelocityMin = _speed / process.ScaleMax;
+				_particles.ProcessMaterial = process;
+				_particles.Lifetime = _path.Length() / (_speed / process.ScaleMax);
+				_particles.Emitting = true;
+            }
+		}
+
+	}
+
 	void PickupObject(Node3D target)
 	{
-		if (target != null ||target is Collectable && _destinationNode != null)
+		if (target != null && target is Collectable && _destinationNode != null)
 		{
 			((Collectable)target).Transport(this);
 		}
